a7f070b94dea55b1b6ff106e32941b13bd3d8a2b
Kim Doo Young <177651948+eudooyoung@users.noreply.github.com>
2026-07-17 12:23:54 +0900

    feat: add basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { Table } from "./components/table/Table.tsx";
+
 function App() {
-  return <>Hello React Template</>;
+  return (
+    <main>
+      <div>
+        <button>라방</button>
+        <button>홈쇼핑</button>
+      </div>
+      <Table />
+    </main>
+  );
 }
 
 export default App;

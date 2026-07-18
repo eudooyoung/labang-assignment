3a4e8c43bcbe8ed544140a963393a9a203bf005d
Kim Doo Young <177651948+eudooyoung@users.noreply.github.com>
2026-07-18 16:24:37 +0900

    feat: add buttons styles

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
   const { list, listLoading, listError, getList } = useList();
   const [listType, setListType] = useState<ListType>("lb");
 
-  if (userLoading) {
+  if (userLoading || loginLoading || logoutLoading) {
     return <>Loading...</>;
   }
 
@@ -23,8 +23,8 @@ function App() {
     e.preventDefault();
     const success = await login();
     if (success) {
-      await getList("lb");
       await getUser();
+      await getList(listType);
     }
   };
 
@@ -32,8 +32,8 @@ function App() {
     e.preventDefault();
     const success = await logout();
     if (success) {
-      await getList("lb");
       await getUser();
+      await getList(listType);
     }
   };
 
@@ -44,30 +44,54 @@ function App() {
 
   return (
     <main className={styles.main}>
-      {userError && <p>{userError.message}</p>}
-      {!user && (
-        <button disabled={loginLoading} onClick={loginHandler}>
-          로그인
-        </button>
-      )}
-      {loginError && <p>{loginError.message}</p>}
-      {user && (
-        <button disabled={logoutLoading} onClick={logoutHandler}>
-          로그아웃
-        </button>
-      )}
-      {logoutError && <p>{logoutError.message}</p>}
-
-      <div>
-        <button onClick={toggleList("lb")}>라방</button>
-        <button onClick={toggleList("hs")}>홈쇼핑</button>
+      {/* 로그인 & 로그아웃 버튼 */}
+      <div className={styles.loginButtonWrapper}>
+        {userError && <p>{userError.message}</p>}
+        {!user && (
+          <button
+            className={styles.loginButton}
+            disabled={loginLoading}
+            onClick={loginHandler}>
+            로그인
+          </button>
+        )}
+        {loginError && <p>{loginError.message}</p>}
+        {user && (
+          <button
+            className={styles.logoutButton}
+            disabled={logoutLoading}
+            onClick={logoutHandler}>
+            로그아웃
+          </button>
+        )}
+        {logoutError && <p>{logoutError.message}</p>}
       </div>
-      {!listLoading && !listError && (
-        <>
-          {listType === "lb" && <LBTable list={list as LiveBroadCast[]} />}
-          {listType === "hs" && <HSTable list={list as HomeShopping[]} />}
-        </>
-      )}
+
+      {/* 타입 토글 */}
+      <div className={styles.toggles}>
+        <button
+          className={`${styles.toggleButton} ${listType !== "lb" ? styles.inActive : ""}`}
+          onClick={toggleList("lb")}>
+          라방
+        </button>
+        <button
+          className={`${styles.toggleButton} ${listType !== "hs" ? styles.inActive : ""}`}
+          onClick={toggleList("hs")}>
+          홈쇼핑
+        </button>
+      </div>
+
+      {/* 테이블 */}
+      <div className={styles.tableWrapper}>
+        {!listLoading && !listError && (
+          <>
+            {listType === "lb" && <LBTable list={list as LiveBroadCast[]} />}
+            {listType === "hs" && <HSTable list={list as HomeShopping[]} />}
+          </>
+        )}
+        {listLoading && <p>Loading...</p>}
+        {listError && <p>{listError.message}</p>}
+      </div>
     </main>
   );
 }
